55ae5ca37f4db395c61f23e0a93823d0e062d890
real.disguised.snake@gmail.com <real.disguised.snake@gmail.com>
2026-05-21 22:54:43 -0400

    e

--- src/functions/functions.py
+++ src/functions/functions.py
@@ -1,4 +1,5 @@
 """Helper functions for other parts of the program"""
+
 """REFACTOR"""
 import configparser
 import subprocess
@@ -6,9 +7,9 @@ import shutil
 import getpass
 import traceback
 import tomllib
+
 config = configparser.ConfigParser()
 config.read("../settings/settings.ini")
-
 
 
 def run(cmd):
@@ -25,28 +26,51 @@ def run(cmd):
         print("\n-Settings-\n")
         print_settings()
         raise SystemExit from e
-    print(f"Ran command, {" ".join(cmd)} with user {getpass.getuser()}")
+    print(f"Ran command, {' '.join(cmd)} with user {getpass.getuser()}")
+
 
 def print_settings():
     """Prints all settings present (used for error handling)"""
     for section in config:
         print(f"[{section}]")
-        for k,v in config.items(section):
+        for k, v in config.items(section):
             print(f"{k} = {v}")
+
+
 def pacman_install(package_names):
     """Installs a list of pacman packages"""
     if getpass.getuser() == "root":
-        command = ["pacman", "-S", "--needed", "--noconfirm",] + package_names
+        command = [
+            "pacman",
+            "-S",
+            "--needed",
+            "--noconfirm",
+        ] + package_names
     elif shutil.which("sudo"):
-        command = ["sudo", "pacman", "-S", "--needed", "--noconfirm",] + package_names
+        command = [
+            "sudo",
+            "pacman",
+            "-S",
+            "--needed",
+            "--noconfirm",
+        ] + package_names
     else:
         print("You dont have sudo manually install via pacman to continue (somehow)")
         raise SystemExit from RuntimeError
     run(command)
+
+
 def flatpak_install(package_names):
     """Install a list of flathub packages"""
-    command = ["flatpak", "install", "--noninteractive", "-y",] + package_names
+    command = [
+        "flatpak",
+        "install",
+        "--noninteractive",
+        "-y",
+    ] + package_names
     run(command)
+
+
 def yay_install(package_names):
     """
     Install a list of packages from the AUR using yay as a wrapper,
@@ -59,39 +83,52 @@ def yay_install(package_names):
         pacman_install(["base-devel", "git"])
         run(["git", "clone", "https://aur.archlinux.org/yay.git"])
         run(["cd yay", "&&", "makepkg -si"])
-    command=["yay", "-S", "--needed", "--noconfirm", "--cleanafter",] + package_names
+    command = [
+        "yay",
+        "-S",
+        "--needed",
+        "--noconfirm",
+        "--cleanafter",
+    ] + package_names
     run(command)
+
 
 def open_packages(dir_name, other: str | None):
     """used for getting a bunch of packages from settings"""
-    packages=f"../settings/packages/{dir_name}"
+    packages = f"../settings/packages/{dir_name}"
     if other is None:
-        packages+=f"/{dir_name}-packages.toml"
+        packages += f"/{dir_name}-packages.toml"
     else:
-        packages+=f"/{other}-packages.toml"
+        packages += f"/{other}-packages.toml"
     try:
         with open(packages, "rb") as file:
-            data=tomllib.load(file)
+            data = tomllib.load(file)
     except PermissionError as e:
         print(f"You seemingly dont have permission to access {e.filename}... \n")
         print(traceback.format_exc)
         raise SystemError from e
-    package_list=[]
+    package_list = []
+
     def walk(d: dict, li: list):
-        for k,v in d.items():
+        for k, v in d.items():
             if k == "notes":
                 continue
-            if isinstance(v, list) and k=="packages":
+            if isinstance(v, list) and k == "packages":
                 for i in v:
                     li.append(i)
             elif isinstance(v, dict):
                 walk(v, li)
             else:
                 continue
+
     walk(data, package_list)
     return package_list
-def exit_from_error(e : Exception):
+
+
+def exit_from_error(e: Exception):
     """Takes an input of an Exception and then raises system exit from that"""
-    print(f"An error has occured! Please report this:\n{traceback.format_exc()}\n with settings:")
+    print(
+        f"An error has occured! Please report this:\n{traceback.format_exc()}\n with settings:"
+    )
     print_settings()
     raise SystemExit from e
